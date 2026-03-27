2273ed31af850f6020817015023ffefc61258825
Alqiya <alqiya358@gmail.com>
2026-03-27 21:44:00 +0530

    Time: 14 ms (46.66%) | Memory: 20.3 MB (18.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict={}
-        t_dict={}
+        sorted_s=sorted(s)
+        sorted_t=sorted(t)
 
-        def string_to_dict(string,dictionary):
-            for letter in string:
-                if letter in dictionary:
-                    dictionary[letter]+=1
-                elif letter==' ':
-                    continue
-                else:
-                    dictionary[letter]=1
-            return dictionary
-            
-        str_s=string_to_dict(s,s_dict)
-        str_t=string_to_dict(t,t_dict)
-
-        return str_s==str_t
+        if sorted_s==sorted_t:
+            return True
+        else:
+            return False
