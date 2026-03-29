741dc8e18e65bbeba0b6cbabde32228cfa0a6b6e
Alqiya <alqiya358@gmail.com>
2026-03-29 11:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (65.69%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -3,12 +3,9 @@ class Solution:
         op={}
         op_list=[]
         for n in nums1:
-            op[n]=op.get(n,0)+1
+            op[n]=1
         for n in nums2:
-            if n in op_list:
-                continue
-            elif n not in op_list:
-                if n in op.keys():
-                    op_list.append(n)
-            
+            if n in op and n not in op_list:
+                op_list.append(n)
         return op_list
+
